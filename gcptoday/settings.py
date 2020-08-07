679acb6d836701af140a82c3a8cbebac934703aa
Irene Wangui <i.wangui@medluxinternational.com>
2020-08-07 13:30:26 +0300

    second commit

--- gcptoday/settings.py
+++ gcptoday/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '3$l0v3(4%qw)*&_@(#66urn$*&gn@7#5^mf3x8**iiice%9t6$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
